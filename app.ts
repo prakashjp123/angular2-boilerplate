c41b5e6ad34e09bdbaf8d02298376fc957b726ae
Jayaprakash <prakashjp.h@gmail.com>
2015-06-26 13:58:52 +0530

    Configuring bower to use angular2

--- app.ts
+++ app.ts
@@ -4,19 +4,24 @@ import { Component, View, bootstrap } from 'angular2/angular2';
 
 // Annotation section
 @Component({
-	selector: 'ng2bp'
+	selector: 'my-app'
 })
 
 @View({
-	template: '<h3>Hello {{ name }}</h3>'
+	template: `
+		<h3>Hello {{ name }}</h3>
+		<p>{{ message }}</p>
+	`
 })
 
 // Component controller
 class MyAppComponent {
 	name: string;
+	message: string;
 	
 	constructor() {
 		this.name = 'Jayaprakash';
+		this.message = 'Welcome to forkIdeas.';
 	}
 }
 
